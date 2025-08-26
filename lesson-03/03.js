f58498c2fe21ed58d12a7d1acda885f111a00e9c
kondrat95 <vmkondratenko70@gmail.com>
2025-08-27 01:47:03 +0200

    unit3.3

--- lesson-03/03.js
+++ lesson-03/03.js
@@ -1,5 +1,5 @@
 // Разработайте функцию findLargest, которая принимает три числа и возвращает наибольшее из них.
 
 function findLargest(num1, num2, num3) {
-    return
+    return Math.max(num1, num2, num3);
 }
