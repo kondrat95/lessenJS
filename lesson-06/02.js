66bff6bbc6a2db8a05942e32ae3f640dc2e22045
kondrat95 <vmkondratenko70@gmail.com>
2025-09-20 18:37:09 +0200

    unut6.2

--- lesson-06/02.js
+++ lesson-06/02.js
@@ -24,4 +24,25 @@ const doubledNumbers = map(numbers, (element, index) => {
 console.log(doubledNumbers) // Должен вывести: [2, 4, 6, 8, 10]
 */
 
-const map = () => {}
+const numbers = [1, 2, 3, 4, 5]
+
+const map = (array,callback) => {
+    let newArray = []
+    for (let i = 0; i < array.length; i++) {
+        const element = array[i];
+
+       let result =  callback(element,i);
+        newArray.push(result);
+
+    }
+
+    return newArray
+}
+
+let callback = (element,index) => {
+    return  element * 2;
+
+
+}
+
+console.log(map(numbers, callback));
