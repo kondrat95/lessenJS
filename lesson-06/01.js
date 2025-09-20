5b3e7e4c5b49a93ee3a317ec767200e6e5f95bbc
kondrat95 <vmkondratenko70@gmail.com>
2025-09-20 17:45:31 +0200

    unut6.1

--- lesson-06/01.js
+++ lesson-06/01.js
@@ -22,5 +22,25 @@ const oddNumbers = filter(numbers, (element, index) => {
 
 console.log(oddNumbers) // Должен вывести: [1, 3, 5]
 */
+const array = [1, 2, 3, 4, 5]
 
-const filter = () => {}
+function filter(array, callback) {
+    const newArray = []
+    for (let i = 0; i < array.length; i++) {
+        let item = array[i];
+
+       const result = callback(item,i)
+
+        if (result){
+            newArray.push(item)
+        }
+    }
+
+return newArray
+}
+
+const sum = (element, index) => {
+    return element > 2;
+}
+
+console.log(filter(array, sum));
